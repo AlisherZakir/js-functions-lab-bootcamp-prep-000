22e7240adfd245764523ff32f94aed926fa18de0
Alisher Zakir <a3zakir@gmail.com>
2019-04-18 13:42:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // write your code below!
-function happyHolidays {
-  return
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysto(name) {
+  return `Happy holidays, ${name}`
+}
+
+function happyHolidayto(holiday, name) {
+  return `Happy ${holiday}, ${name}`
+}
+
+function holidayCountdown(holiday, day) {
+  return `It's ${days} days until ${holiday}!`
 }
